37a089af95c6df785bdf2d7ce9cc5d7d2f5bae6a
Oyiti <emmanueloyiti2@gmail.com>
2023-06-20 10:52:53 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Description: Prints the lowercase alphabet 10 times using _putchar
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
